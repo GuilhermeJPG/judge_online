178cf1ef0dbe9cf335418663d9ea8024572deb1a
GuiJPG <oguilherme.rodrigues10@gmail.com>
2024-12-04 21:34:32 -0300

    Ex1003-js

---
+++ Beginner/JavaScript/Ex1003.js
@@ -0,0 +1,13 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var linhas = input.split(" ");
+
+console.log("Array: " + linhas); //Exibir o Array de linhas
+var a = parseInt(linhas.shift()); 
+console.log("Removeu o primeiro elemento do Array:" + a);
+console.log("Array atualizado:" + linhas);
+var b = parseInt(linhas.shift());
+console.log("Removeu o primeiro elemento do Array atualizado:" + b);
+
+var soma = (a + b);
+
+console.log("SOMA = " + soma)
