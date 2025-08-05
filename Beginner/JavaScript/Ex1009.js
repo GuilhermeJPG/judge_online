4cc4908f6e00cc8e41d7211a102bf913e85995f6
Guilherme <guilherme@MacBook-Pro-de-Guilherme.local>
2025-08-05 17:22:26 -0300

    updated

--- Beginner/JavaScript/Ex1009.js
+++
@@ -1,9 +0,0 @@
-var input = require("fs").readFileSync("stdin", "utf-8");
-var linha = input.split(" ")
-
-var nomeVendedor = linha.shift();
-var salary = parseFloat(linha.shift());
-var totalVendas = parseFloat(linha.shift());
-
-var somaSalario = (totalVendas * 0.15) + (salary);
-console.log("TOTAL = R$ " + somaSalario.toFixed(2));
--- Beginner/JavaScript/Ex1016.js
+++
@@ -1,8 +0,0 @@
-const input = require("fs").readFileSync("stdin", "utf-8");
-const lines = input.split("\n");
-
-const distancia = lines.shift();
-
-const tempo = distancia * 2;
-
-console.log(`${tempo} minutos`);
